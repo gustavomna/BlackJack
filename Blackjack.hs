b5bc6c5665f4872e6a24aa0d9a0b5c2715b52913
Marcos Dourado <marcosdourado92@gmail.com>
2017-04-10 23:29:07 -0300

    created the function that draws the dealer’s cards

--- Blackjack.hs
+++ Blackjack.hs
@@ -1,1 +1,10 @@
-data Cards = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K
+import System.Random
+
+data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K
+
+entregarCartasDealer :: StdGen -> Int -> [Cartas]
+entregarCartasDealer gen total
+  | total < 11 && randomNum == 1 toEnum(randomNum - 1) : entregarCartasDealer novoGen (total + randomNum + 10)
+  | total < 17 = toEnum (randomNum - 1) : entregarCartasDealer novoGen (total + randomNum)
+  | otherwise = []
+  where (randomNum, novoGen) = random(1,13) gen :: (Int, StsGen)
