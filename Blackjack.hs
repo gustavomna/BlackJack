0d663b862b6643934ab0f5bda82c275b7ac767cf
Marcos Dourado <marcosdourado92@gmail.com>
2017-04-11 23:59:54 -0300

    created somaCartas and apontarTotal functions

--- Blackjack.hs
+++ Blackjack.hs
@@ -1,6 +1,13 @@
+import Text.Read
+import Data.Char 
+import System.IO
+import qualified Data.Text.IO
+import Data.List
+import System.Environment
 import System.Random
+import qualified Data.Map
 
-data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K
+data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K deriving (Show, Enum, Bounded, Read)
 
 -- Entrega cartas do dealer. Até o valor ser 17 ou superior.
 entregarCartasDealer :: StdGen -> Int -> [Cartas]
@@ -13,14 +20,14 @@ entregarCartasDealer gen total
 -- Entrega cartas até jogador parar
 iniciarCartasJogador :: StdGen -> Int -> [Cartas]
 iniciarCartasJogador gen parar
-  |parar \= 0 = toEnum(randomNum - 1) : iniciarCartasJogador novoGen (parar - 1)
+  |parar /= 0 = toEnum(randomNum - 1) : iniciarCartasJogador novoGen (parar - 1)
   |otherwise = []
-  where (randomNum, newGen) = randomR(1,13) gen :: (Int, StdGen)
+  where (randomNum, novoGen) = randomR(1,13) gen :: (Int, StdGen)
 
 -- Entrega uma carta para quem chamar
 entregarCartaJogador :: StdGen -> [Cartas]
 entregarCartasJogador gen = [toEnum (randomNum - 1)]
-  where (randomNum, newGen) = randomR(1,13) gen :: (Int, StdGen)
+  where (randomNum, novoGen) = randomR(1,13) gen :: (Int, StdGen)
 
 -- recebe um vetor de cartas e retorna um vetor de inteiros contendo os valores
 -- dessas cartas
@@ -42,7 +49,24 @@ mostrarMao :: [Cartas] -> [Int]
 mostrarMao [] = []
 mostrarMao (c:cx) = (fromEnum (c) + 1) : mostrarMao cx
 
---converte os valores dos inteiros para Cartas (11 =  J, 13 = K) 
+-- converte os valores dos inteiros para Cartas (11 =  J, 13 = K) 
 displayCard :: [Int] -> [Cartas]
 displayCard [] = []
 displayCard (x:xs) = toEnum (x - 1) : displayCard xs
+
+-- soma os valores das cartas que estao em um vetor de inteiros, se o 'A' estiver nesse vetor e 
+-- a soma total for menor que 12, entao o 'A' valera 11
+somaCartas :: [Int] -> Int
+somaCartas cartas
+	| sum cartas < 12 && 1 `elem` cartas = sum (cartas) + 10
+  | otherwise = sum cartas
+
+-- mostra resultado do jogo
+apontarTotal :: [Cartas] -> [Cartas] -> String
+apontarTotal cartasDealer cartasJogador
+  | somaCartas (valorDaMao cartasDealer) == somaCartas (valorDaMao cartasJogador) = "Jogo Empatado"
+  | somaCartas (valorDaMao cartasDealer) > somaCartas (valorDaMao cartasJogador) = "Voce Perdeu"
+  | somaCartas (valorDaMao cartasDealer) < somaCartas (valorDaMao cartasJogador) = "Voce Perdeu"
+  
+
+
