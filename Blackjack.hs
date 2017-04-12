f027c7975e3e7e0a85887d75cd207dd72ea8a32d
Thiago Lima <thiagolima.unb@gmail.com>
2017-04-12 11:13:57 -0300

    creating logic of the game

--- Blackjack.hs
+++ Blackjack.hs
@@ -9,6 +9,105 @@ import qualified Data.Map
 
 data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K deriving (Show, Enum, Bounded, Read)
 
+jogo :: [Cartas] -> [Cartas] -> Double -> Double -> Double -> IO()
+jogo cartasDealer cartasJogador saldoAtual saldoObjetivo aposta = do
+  --  confere se o jogador ganhou o jogo
+  if saldoAtual >= saldoObjetivo
+    then putStrLn $ "Parabens, voce conseguiu " ++ show saldoAtual ++ " e seu objetivo era " ++ saldoObjetivo
+  -- se ainda nao ganhou o jogo
+  -- confere se a aposta é valida
+  else if bet == 0
+    then do putStrLn $ "Voce tem " ++ show saldoAtual ++ " / " ++ show saldoObjetivo
+            putStrLn $ "Faca a aposta para o proximo jogo: \n"
+            valorDaAposta <- getLine
+            -- valida se a aposta é negativa ou é mais do que o saldo do jogador
+            if (read valorDaAposta::Double) < 1 || (read valorDaAposta::Double) > saldoAtual
+              then do putStrLn "Aposta invalida, aposte um valor entre 0 e o seu saldo"
+                      jogo cartasDealer cartasJogador saldoAtual saldoObjetivo 0
+            else do putStrLn $ "Aposta aceita\n"
+                    jogo cartasDealer cartasJogador saldoAtual saldoObjetivo (read valorDaMao::Double)
+  -- confere se o dealer passou de 21
+  else if (somaCartas $ valorDaMao cartasDealer) > 21
+      then do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+              putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+              putStrLn $ "Dealer passou de 21, voce ganhou"
+              -- e é isso??
+              dealerGen <- newStdGen
+              jogadorGen <- newStdGen
+              jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) (saldoAtual + aposta) saldoObjetivo 0
+  -- confere se o jogador passou de 21
+  else if (somaCartas $ valorDaMao cartasJogador) > 21
+    then do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+            putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+            putStrLn $ "Voce passou de 21, voce perdeu"
+            -- confere se o jogador esta abaixo do saldo
+            if (saldoAtual - aposta) < 1
+              then putStrLn "Voce perdeu todo seu dinheiro. O jogo acabou"
+            else do
+              dealerGen <- newStdGen
+              jogadorGen <- newStdGen
+              jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) (saldoAtual - aposta) saldoObjetivo 0
+  -- se o jogador conseguiu um 21
+  else if (somaCartas $ valorDaMao cartasJogador) == 21
+    then do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+            putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+            putStrLn $ "Voce conseguiu um BlackJack! Sua aposta vale 1.5 vezes"
+            dealerGen <- newStdGen
+            jogadorGen <- newStdGen
+            jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) (saldoAtual + aposta + (aposta / 2)) saldoObjetivo 0
+  -- se deu empate
+  else if (somaCartas $ valorDaMao cartasJogador) == (somaCartas $ valorDaMao cartasDealer)
+    then do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+            putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+            putStrLn $ "Empate! Voce nao ganhou nem perdeu dinheiro"
+            dealerGen <- newStdGen
+            jogadorGen <- newStdGen
+            jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) saldoAtual saldoObjetivo 0
+  -- se ...
+  else do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+          putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+          -- jogador decide se quer mais uma carta ou nao
+          putStrLn "Voce quer MAIS ou quer PARAR?\n"
+          escolha <- getLine
+          -- gera nova carta e chama a funcao jogo
+          if map toLower escolha == "mais"
+            then do jogadorGen <- newStdGen
+                    let novaCarta = entregarCartaJogador jogadorGen
+                    let novasCartasDoJogador = cartasJogador ++ novaCarta
+                    jogo cartasDealer novasCartasDoJogador saldoAtual saldoObjetivo aposta
+          else if map toLower escola == "parar"
+            -- voce ganhou
+            then if (apontarTotal cartasDealer cartasJogador) == "Voce Ganhou"
+              then do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+                      putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+                      putStrLn "Voce tem valor maior, voce ganhou"
+                      dealerGen <- newStdGen
+                      jogadorGen <- newStdGen
+                      jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) (saldoAtual + aposta) saldoObjetivo 0
+            -- voce perdeu
+            else if (apontarTotal cartasDealer cartasJogador) == "Voce Perdeu"
+              then do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+                      putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+                      putStrLn $ "Dealer tem valor maior, voce perdeu"
+                      -- confere se o jogador esta abaixo do saldo
+                      if (saldoAtual - aposta) < 1
+                        then putStrLn "Voce perdeu todo seu dinheiro. O jogo acabou"
+                      else do
+                        dealerGen <- newStdGen
+                        jogadorGen <- newStdGen
+                        jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) (saldoAtual - aposta) saldoObjetivo 0
+            -- empate
+            else do putStrLn $ "Cartas do Dealer: " ++ mostrarCarta cartasDealer ++ "\n"
+                    putStrLn $ "Suas cartas: " ++ mostrarCarta cartasJogador ++ "\n"
+                    putStrLn $ "Empate! Voce nao ganhou nem perdeu dinheiro"
+                    dealerGen <- newStdGen
+                    jogadorGen <- newStdGen
+                    jogo (entregarCartasDealer dealerGen 0) (iniciarCartasJogador jogadorGen 2) saldoAtual saldoObjetivo 0
+          --  escolha invalida
+          else do putStrLn "Escolha se quer MAIS ou quer PARAR\n"
+            jogo cartasDealer cartasJogador saldoAtual saldoObjetivo aposta
+
+
 -- Entrega cartas do dealer. Até o valor ser 17 ou superior.
 entregarCartasDealer :: StdGen -> Int -> [Cartas]
 entregarCartasDealer gen total
