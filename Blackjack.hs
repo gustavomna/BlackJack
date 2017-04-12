73034d55c4504707efdfa68c60bcbdd9dd89539c
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-04-11 22:16:04 -0300

    Fixing entregaCartas function

--- Blackjack.hs
+++ Blackjack.hs
@@ -4,7 +4,7 @@ data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez
 
 entregarCartasDealer :: StdGen -> Int -> [Cartas]
 entregarCartasDealer gen total
-  | total < 11 && randomNum == 1 toEnum(randomNum - 1) : entregarCartasDealer novoGen (total + randomNum + 10)
-  | total < 17 = toEnum (randomNum - 1) : entregarCartasDealer novoGen (total + randomNum)
-  | otherwise = []
-  where (randomNum, novoGen) = random(1,13) gen :: (Int, StsGen)
+  |total < 11 && randomNum == 1 = toEnum(randomNum - 1) : entregarCartasDealer novoGen (total + randomNum + 10)
+  |total < 17 = toEnum (randomNum - 1) : entregarCartasDealer novoGen (total + randomNum)
+  |otherwise = []
+  where (randomNum, novoGen) = random(1,13) gen :: (Int, StdGen)
