ff4b52d46efc86816201914aca5632db6a9e25f9
Gustavo Moreira <gustavomn93@gmail.com>
2017-04-13 22:44:24 -0300

    Adding main function

--- Blackjack.hs
+++ Blackjack.hs
@@ -10,6 +10,20 @@ import System.Exit
 
 data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K deriving (Show, Enum, Bounded, Read)
 
+main = do
+  putStrLn "Digite o seu objetivo de saldo para ganhar, (O mínimo do saldo limite da casa eh 20 reais, a aposta minima eh de 10 reais) :\n"
+  saldo <- getLine
+  if (read saldo::Double) > 19
+    then menu ((read saldo::Double) / 2::Double) (read saldo::Double)
+    else do putStrLn "Entre no minimo um objetivo de 20 reais"
+            main
+
+  putStrLn "Game over, deseja jogar novamente? \"Sim\" ou \"Nao\"?"
+  escolha <- getLine
+  if map toLower escolha == "sim"
+    then do main
+  else return ()
+
 menu :: Double -> Double -> IO()
 menu saldoAtual saldoObjetivo = do
   putStrLn "Bem vindo ao BalckJack! digite \"Jogar\" para começar o jogo, \"Regras\" ou \"Sair\" para sair do jogo"
@@ -28,9 +42,6 @@ menu saldoAtual saldoObjetivo = do
                   jogo (entregarCartasDealer dealerGen total) (iniciarCartasJogador dealerGen total) saldoAtual saldoObjetivo aposta
   else return ()
 
-
-
-    
 jogo :: [Cartas] -> [Cartas] -> Double -> Double -> Double -> IO()
 jogo cartasDealer cartasJogador saldoAtual saldoObjetivo aposta = do
   --  confere se o jogador ganhou o jogo
