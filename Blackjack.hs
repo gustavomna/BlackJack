49d5cb55680fddacfa1928a93603912ff69f5982
Gabriel Araújo <gabrieldea.silva@gmail.com>
2017-04-15 09:28:25 -0300

    Improve main function

--- Blackjack.hs
+++ Blackjack.hs
@@ -11,18 +11,20 @@ import System.Exit
 data Cartas = A | Dois | Tres | Quatro | Cinco | Seis | Sete | Oito | Nove | Dez | J | Q | K deriving (Show, Enum, Bounded, Read)
 
 main = do
-  putStrLn "Digite o seu objetivo de saldo para ganhar, (O mínimo do saldo limite da casa eh 20 reais, a aposta minima eh de 10 reais) :\n"
+  putStrLn "Digite o seu objetivo de saldo para ganhar, (O mínimo do saldo limite da casa eh 20 reais, a aposta minima eh de 10 reais):"
   saldo <- getLine
-  if (read saldo::Double) > 19
-    then menu ((read saldo::Double) / 2::Double) (read saldo::Double)
-    else do putStrLn "Entre no minimo um objetivo de 20 reais"
-            main
+  if (read saldo) > 19
+    then
+      menu ((read saldo) / 2) (read saldo)
+    else do
+      putStrLn "Entre no minimo um objetivo de 20 reais\n"
+      main
 
   putStrLn "Game over, deseja jogar novamente? \"Sim\" ou \"Nao\"?"
   escolha <- getLine
   if map toLower escolha == "sim"
-    then do main
-  else return ()
+    then main
+    else return ()
 
 menu :: Double -> Double -> IO()
 menu saldoAtual saldoObjetivo = do
